e8a962228da1786240e32dd67c94591034495066
Mads Kristensen <post@madskristensen.net>
2015-11-03 11:39:15 -0800

    Cleaned up

--- src/VSPackage.cs
+++ src/VSPackage.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Windows;
@@ -56,7 +55,7 @@ namespace WebExtensionPack
             var manager = (IVsExtensionManager)GetService(typeof(SVsExtensionManager));
 
             var installed = manager.GetInstalledExtensions();
-            var productIds = GetProductIds();
+            var productIds = ExtensionList.ProductIds();
             var missing = productIds.Where(id => !installed.Any(ins => ins.Header.Identifier == id));
 
             var progress = new InstallerProgress(missing.Count(), $"Downloading and installing {missing.Count()} extension(s)...");
@@ -79,20 +78,6 @@ namespace WebExtensionPack
             }
         }
 
-        private IEnumerable<string> GetProductIds()
-        {
-            return new[] {
-                "5fb7364d-2e8c-44a4-95eb-2a382e30fec9", // Web Essentials
-                "148ffa77-d70a-407f-892b-9ee542346862", // Web Compiler
-                "36bf2130-106e-40f2-89ff-a2bdac6be879", // Web Analyzer
-                "bf95754f-93d3-42ff-bfe3-e05d23188b08", // Image optimizer
-                "950d05f7-bb25-43ce-b682-44b377b5307d", // Glyphfriend
-                "6ed6c371-5815-407f-9148-f64b3a025dd9", // Bootstrap Snippet Pack
-                "f4ab1e64-5d35-4f06-bad9-bf414f4b3bbb", // Open Command Line
-                "fdd64809-376e-4542-92ce-808a8df06bcc", // Package Installer
-            };
-        }
-
         private static void InstallExtension(IVsExtensionRepository repository, IVsExtensionManager manager, string id)
         {
             try
@@ -105,7 +90,7 @@ namespace WebExtensionPack
                 IInstallableExtension installable = repository.Download(entry);
                 manager.Install(installable, false);
 
-                Telemetry.TrackEvent($"Install {installable.Header.Name}");
+                Telemetry.TrackEvent(installable.Header.Name);
             }
             catch (Exception ex)
             {
